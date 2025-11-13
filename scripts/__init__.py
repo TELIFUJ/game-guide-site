cf5cffe8c22d1ac270a76b3a1559a506ee478205
TELIFUJ <TELIFUJMO@GMAIL.COM>
2025-11-14 02:56:04 +0800

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,3 @@
-# make 'scripts' an importable package
+# scripts/__init__.py
+"""Make 'scripts' a package and expose common helpers."""
+from .common_image import normalize_bgg_image_url  # optional re-export
